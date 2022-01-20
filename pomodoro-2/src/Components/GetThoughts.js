d50ce97bbeab61c8fb14ec659947b146bf7b2e9e
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-01-20 12:00:53 +0000

    GetThoughts

--- pomodoro-2/src/Components/GetThoughts.js
+++ pomodoro-2/src/Components/GetThoughts.js
@@ -1,35 +1,51 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 function GetThoughts(props) {
+
+  const [dirty, setDirty] = useState(true)
+
   const [returnedThoughts, setReturnedThoughts] = useState([]);
 
-  const GetThoughts = async () => {
-    try {
-      const response = await fetch(
-        "https://sharp-easley-01b623.netlify.app/api",
-        {
-          headers: { "content-type": "application/json" },
-          // body: JSON.stringify()
-        }
-      );
-
-      const data = await response.json();
-      setReturnedThoughts(data);
-
-      console.log(response);
-    } catch (error) {
-      console.error(error.message);
+  useEffect(() => {
+    if (dirty) {
+      const thoughtsData = async () => {
+        const response = await fetch(
+          "https://pomodoro-project-19.herokuapp.com/thoughts",
+        );
+        const data = await response.json();
+        console.log(data)
+        setReturnedThoughts(data);
+        setDirty(false)
+      };
+      thoughtsData();
     }
-  };
+
+    
+  }, [dirty]);
+
+
+  // const handleclick = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://pomodoro-project-19.herokuapp.com/thoughts",
+  //       {
+  //         headers: { "content-type": "application/json" },
+  //         // body: JSON.stringify()
+  //       }
+  //     );
+  
+  function handleclick() {
+    setDirty(true)
+  }
 
   return (
     <Fragment>
-      <button className="getthoughts" type="submit" onClick={GetThoughts}>
+      <button className="getthoughts" type="submit" onClick={handleclick}>
         Get my thoughts
       </button>
       {returnedThoughts}
     </Fragment>
   );
-}
+};
 
 export default GetThoughts;
