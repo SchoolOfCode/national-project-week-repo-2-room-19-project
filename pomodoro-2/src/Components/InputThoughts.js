9f3288b04a84fbb263375ae9a6e8f977d21f460a
Yeseul Lee <dev.yezlee@gmail.com>
2022-01-20 09:56:57 +0000

    Test for connecting with Heroku/Netlify

--- pomodoro-2/src/Components/InputThoughts.js
+++ pomodoro-2/src/Components/InputThoughts.js
@@ -1,40 +1,44 @@
 import React, { Fragment, useState } from "react";
 
 function InputThoughts() {
+  const [description, setDescription] = useState("");
 
-    const [description, setDescription] = useState("");
-
-    async function onSubmitForm(e) {
-        e.preventDefault();
-        try {
-            const body = {description};
-            console.log(`submitting ${JSON.stringify(body)}`)
-            const response = await fetch("http://localhost:3001/thoughts/", {
-                method: "POST",
-                headers: { "content-type": "application/json" },
-                body: JSON.stringify(body)
-            })
-
-            console.log(response)
-        } catch (error) {
-            console.error(error.message)
+  async function onSubmitForm(e) {
+    e.preventDefault();
+    try {
+      const body = { description };
+      console.log(`submitting ${JSON.stringify(body)}`);
+      const response = await fetch(
+        "https://pomodoro-project-19.herokuapp.com/thoughts",
+        {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(body),
         }
-    }
+      );
 
-    return (
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  return (
     <Fragment>
-        <h1 className="text-center mt-5"> Thoughts </h1>
-        <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-            <input 
-            type="text" 
-            className="form-control" 
-            value={description} 
-            onChange={e => setDescription(e.target.value)}    
-            />
-            <button className="btn btn-success" type="submit">Please add thoughts here</button>
-        </form>
+      <h1 className="text-center mt-5"> Thoughts </h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success" type="submit">
+          Please add thoughts here
+        </button>
+      </form>
     </Fragment>
-    );
+  );
 }
 
 export default InputThoughts;
